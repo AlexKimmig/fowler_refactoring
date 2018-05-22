3ac58881b0dd43c0d436ee7523ab25fdc8873f64
KimmigAlex <Kimmig.Alex@gmail.com>
2018-05-22 17:10:22 +0200

    Moved amountFor from Customer to getCosts in Rental

--- src/Rental.java
+++ src/Rental.java
@@ -1,14 +1,37 @@
 class Rental {
     private Movie movie;
     private int daysRented;
+
     public Rental(Movie newmovie, int newdaysRented) {
         movie = newmovie;
         daysRented = newdaysRented;
     }
+
     public int getDaysRented() {
         return daysRented;
     }
+
     public Movie getMovie() {
         return movie;
+    }
+
+    public double getCosts(Rental rental) {
+        double result = 0;
+        switch (rental.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (rental.getDaysRented() > 2)
+                    result += (rental.getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += rental.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (rental.getDaysRented() > 3)
+                    result += (rental.getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return result;
     }
 }
