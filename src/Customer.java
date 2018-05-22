84cd3d831b1da9c2e6e7aeb93e45a57e586dd89e
KimmigAlex <Kimmig.Alex@gmail.com>
2018-05-22 17:49:31 +0200

    Removed temp variable frequentRenterPoints from method statement(). Replaced it with new getTotelFrequentRenterPoints() method in result

--- src/Customer.java
+++ src/Customer.java
@@ -19,7 +19,6 @@ class Customer {
     }
 
     public String statement() {
-        int frequentRenterPoints = 0;
         Enumeration rentals = this.rentals.elements();
         String result = "Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "Days" + "\t" + "Amount" + "\n";
@@ -27,15 +26,22 @@ class Customer {
         while (rentals.hasMoreElements()) {
             Rental currentRental = (Rental) rentals.nextElement();
 
-            // add frequent renter points
-            frequentRenterPoints += currentRental.getFrequentRentalPoints();
-
             //show figures for this rental
             result += "\t" + currentRental.getMovie().getTitle() + "\t" + "\t" + currentRental.getDaysRented() + "\t" + String.valueOf(currentRental.getCosts()) + "\n";
         }
         //add footer lines
         result += "Amount owed is " + String.valueOf(getTotalCosts()) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
+        return result;
+    }
+
+    private int getTotalFrequentRenterPoints(){
+        int result = 0;
+        Enumeration rentals = this.rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental currentRental = (Rental) rentals.nextElement();
+            result += currentRental.getFrequentRentalPoints();
+        }
         return result;
     }
 
