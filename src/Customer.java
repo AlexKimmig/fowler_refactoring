993f9e9be6588c4fa795ea2e46df5933c01f78aa
KimmigAlex <Kimmig.Alex@gmail.com>
2018-05-22 17:16:02 +0200

    renamed variable each in statement() to currentRental

--- src/Customer.java
+++ src/Customer.java
@@ -27,16 +27,16 @@ class Customer {
 
         while (enum_rentals.hasMoreElements()) {
             double thisAmount = 0;
-            Rental each = (Rental) enum_rentals.nextElement();
+            Rental currentRental = (Rental) enum_rentals.nextElement();
             //determine amounts for each line
-            each.getCosts(each);
+            thisAmount += currentRental.getCosts(currentRental);
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+            if ((currentRental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && currentRental.getDaysRented() > 1)
                 frequentRenterPoints++;
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
+            result += "\t" + currentRental.getMovie().getTitle() + "\t" + "\t" + currentRental.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
             totalAmount += thisAmount;
         }
         //add footer lines
